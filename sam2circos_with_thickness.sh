4551e519d8519a89f08e09f37eda5bad7d454c6a
S. Dean Rider Jr <94093691+DeanRider@users.noreply.github.com>
2022-07-18 00:11:27 -0400

    Update sam2circos_with_thickness.sh

--- sam2circos_with_thickness.sh
+++ sam2circos_with_thickness.sh
@@ -11,7 +11,7 @@
 # will then be printed out on a per line basis.
 # Requires a custom karyotype file to be used with Circos.
 # Some data lines will be messed up and manual curation is required.
-# use: bash sam2sircos_with_thickness.sh
+# use: bash sam2circos_with_thickness.sh
 # written July 10, 2022 by S. Dean Rider Jr. based on the following:
 # https://www.unix.com/unix-for-dummies-questions-and-answers/179555-count-different-characters-one-column.html
 # answer by scrutinizer
